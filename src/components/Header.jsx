7288e6953db63171a8c9fe477652816c0bab37a8
Imaad Uddin <imaaduddin27@gmail.com>
2024-12-08 01:01:03 -0600

    tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header>
+    <header className=''>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
